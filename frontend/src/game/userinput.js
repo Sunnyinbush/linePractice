69d746114e76acf069fbc249a6c6cc128da17497
Punnapak <pupunbolt@gmail.com>
2023-02-19 14:23:28 +0700

    feat: upload image

--- frontend/src/game/userinput.js
+++ frontend/src/game/userinput.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import mainLogo from'./KrD_log.png';
 import topimg from './top_image.png'
+import botimg from './bottom.png'
 import './main.css';
 import  './userinput.css'
+
 import { Container,Row ,Button} from 'react-bootstrap';
 
 export default function Userinput() {
@@ -29,14 +31,18 @@ export default function Userinput() {
 
       <Container>
         <div class='box'>
-
+          <Row>
           <p>ระยะเวลาที่คุณมี</p>
 
-          <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" />
+        <div class="slidecontainer">
+          <input type="range" min="1" max="100" value="50" class="slider" />
 
-          </div>
+        </div>
 
+
+            
+          </Row>
+   
 
         </div>
         </Container>
@@ -46,6 +52,14 @@ export default function Userinput() {
         < div class='seacrh'>
           <Button> Click</Button>
         </div>
+
+
+        <Container>
+          <div class='center'>
+              <img src={botimg} class='bot'/>
+        
+          </div>
+        </Container>
       
         
     </div>
