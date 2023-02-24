499635d3497900b1ea14cae8e5ce3df9def4f999
Chaianun Uaramnueyporn <jimmy21ishandsome@gmail.com>
2023-02-24 15:40:13 +0700

    feat: send flex separately from user and group

--- backend/controllers/messageController/sendFlexController.js
+++ backend/controllers/messageController/sendFlexController.js
@@ -4,7 +4,7 @@ const config = {
   channelAccessToken: 'vqiiCUBQCIZS0AJwgAunwn47nyFlDemUcsx9fr7SPXIQmXIlbg17N5hm0z+zo1Kcgges2TvHcKRoDuKBFiF0rAHuqE7TBQNYFlk1Q+Mkq2UyymeyWA249Abektv8lnES71cNhVO1LdOQiqsX/kx5ywdB04t89/1O/w1cDnyilFU=',
   channelSecret: 'ca2d11dc313463c0420dce1998398dfa',
 };
-
+const sendToDb = require('../liffControllers/foodController');
 const client = new line.Client(config);
 
 // Send Flex Message
@@ -13,24 +13,29 @@ const sendFlexMessage = async (req, res) => {
   events.forEach(async (event) => {
     if (event.type === 'message' && event.message.type === 'text' && event.message.text === 'กินไรดี'||'กินอะไรดี') {
       console.log(event);
-      // Construct the Flex message object using the retrieved data
-      const message = {
-        type: 'flex',
-        altText: 'This is a Flex Message',
-        contents: flexMessageData,
-      };
-
       if (event.source.type === 'group') {
+        // Construct the Flex message object using the retrieved data
+        const message = {
+          type: 'flex',
+          altText: 'This is a Flex Message',
+          contents: flexMessageData['groupflex'],
+        };
         groupId = event.source.groupId;
         // Send the Flex message to the group
         return client.pushMessage(groupId, message);
       } else if (event.source.type === 'user') {
+        // Construct the Flex message object using the retrieved data
+        const message = {
+          type: 'flex',
+          altText: 'This is a Flex Message',
+          contents: flexMessageData['userflex'],
+        };
         userId = event.source.userId;
         // Send the Flex message to the user
         return client.pushMessage(userId, message);
       }
     } else{
-      
+
     }
   });
 
