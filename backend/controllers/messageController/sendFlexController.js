5152d84d2e6d487a78877225134090348525a0ff
Vorapon Kachapornkul <hakao97@gmail.com>
2023-02-25 00:09:08 +0700

    feat: change from push to reply message

--- backend/controllers/messageController/sendFlexController.js
+++ backend/controllers/messageController/sendFlexController.js
@@ -1,4 +1,4 @@
-const {flexMessageData} = require('../../models/sendFlexModels.js');
+const { flexMessageData } = require('../../models/sendFlexModels.js');
 const line = require('@line/bot-sdk');
 const config = {
   channelAccessToken: 'vqiiCUBQCIZS0AJwgAunwn47nyFlDemUcsx9fr7SPXIQmXIlbg17N5hm0z+zo1Kcgges2TvHcKRoDuKBFiF0rAHuqE7TBQNYFlk1Q+Mkq2UyymeyWA249Abektv8lnES71cNhVO1LdOQiqsX/kx5ywdB04t89/1O/w1cDnyilFU=',
@@ -10,9 +10,9 @@ const client = new line.Client(config);
 // Send Flex Message
 const sendFlexMessage = async (req, res) => {
   const { events } = req.body;
-  events.forEach(async (event) => {
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
     if (event.type === 'message' && event.message.type === 'text' && (event.message.text === 'กินไรดี' || event.message.text === 'กินอะไรดี')) {
-      console.log(event);
       if (event.source.type === 'group') {
         // Construct the Flex message object using the retrieved data
         const message = {
@@ -22,7 +22,7 @@ const sendFlexMessage = async (req, res) => {
         };
         groupId = event.source.groupId;
         // Send the Flex message to the group
-        return client.pushMessage(groupId, message);
+        await client.replyMessage(event.replyToken, message);
       } else if (event.source.type === 'user') {
         // Construct the Flex message object using the retrieved data
         const message = {
@@ -32,16 +32,12 @@ const sendFlexMessage = async (req, res) => {
         };
         userId = event.source.userId;
         // Send the Flex message to the user
-        return client.pushMessage(userId, message);
+        await client.replyMessage(event.replyToken, message);
       }
-    } else{
-
     }
-  });
-
+  }
   res.sendStatus(200);
 };
-
 
 module.exports = {
   sendFlexMessage,
