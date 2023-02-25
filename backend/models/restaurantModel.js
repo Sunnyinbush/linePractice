c77c23bac779c5ddf1c4a6628325d27f8a4c1b0f
Vorapon Kachapornkul <hakao97@gmail.com>
2023-02-25 22:12:26 +0700

    fix: add return from axios

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -1,28 +1,26 @@
 const axios = require('axios');
 
-const getRestaurantData = (req, res) => {
-  axios.get('http://localhost:9000/restaurantData')
-    .then(response => {
-      res.status(200).json({ status: 'success', data: response.data });
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Internal server error');
-    });
+const getRestaurantData = async () => {
+  try {
+    const response = await axios.get('http://localhost:9000/restaurantData');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return { error: 'Internal server error' };
+  }
 }
 
-const getgameState = (req, res) => {
-  axios.get('http://localhost:9000/gameState')
-    .then(response => {
-      res.status(200).json({ status: 'success', data: response.data });
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Internal server error');
-    });
+const getGameState = async () => {
+  try {
+    const response = await axios.get('http://localhost:9000/gameState');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return { error: 'Internal server error' };
+  }
 }
 
 module.exports = {
   getRestaurantData,
-  getgameState
+  getGameState
 };
