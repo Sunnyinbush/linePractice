db4f5426cc1ed5716e6bec7b248cb464afa19a0a
Vorapon Kachapornkul <hakao97@gmail.com>
2023-02-25 22:06:51 +0700

    fix: make sure axios returns models

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -1,24 +1,25 @@
-//get all restaurantdata in database.json in jsonServer at local host 9000 and export the module using axios
 const axios = require('axios');
 
-const getRestaurantData = async (req, res) => {
-  try {
-    const response = await axios.get('http://localhost:9000/restaurantData');
-    res.status(200).json({ status: 'success', data: response.data });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal server error');
-  }
+const getRestaurantData = (req, res) => {
+  axios.get('http://localhost:9000/restaurantData')
+    .then(response => {
+      res.status(200).json({ status: 'success', data: response.data });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    });
 }
 
-const getgameState = async (req, res) => {
-  try {
-    const response = axios.get('http://localhost:9000/gameState').then((response) => {
-    res.status(200).json({ status: 'success', data: response.data })});
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal server error');
-  }
+const getgameState = (req, res) => {
+  axios.get('http://localhost:9000/gameState')
+    .then(response => {
+      res.status(200).json({ status: 'success', data: response.data });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    });
 }
 
 module.exports = {
