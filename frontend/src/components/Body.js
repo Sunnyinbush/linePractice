bfbe4a687d1bdfda9518caf1007eaf372cf14809
Tt <tt.taiton@gmail.com>
2023-02-25 14:21:27 +0700

    feat: change the body to be static

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,122 +1,110 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+// import Progress from "./progress";
 
 function Body({ picture }) {
   const [selectedImgIndex, setSelectedImgIndex] = useState(-1);
   const [count, setCount] = useState(0);
   const [imgIndexes, setImgIndexes] = useState([]);
-  const [list1,setList1] = useState([]);
+  const [list,setList] = useState([]);
   const [list2,setList2] = useState([]);
   const [list3,setList3] = useState([]);
   const [list4,setList4] = useState([]);
   const [list5,setList5] = useState([]);
+  const [borderIndex,setBorderIndex] = useState(null);
   const [counting,setCounting] = useState(0);
+  
 
   
-  const handleImgClick = (index, src) => {
-    const picLink = "localhost:3000" + src;
-    setSelectedImgIndex(index);
-    setCounting((prevCounting) => prevCounting + 1);
-    
-    if (list1.length === 0) {
-      setList1([picLink]);
-    } else if (list2.length === 0) {
-      setList2([picLink]);
-    } else if (list3.length === 0) {
-      setList3([picLink]);
-    } else if (list4.length === 0) {
-      setList4([picLink]);
-    } else if (list5.length === 0) {
-      setList5([picLink]);
-    } else if (list1.length === 1) {
-      setList2((list) => [...list, picLink]);
-    } else if (list2.length === 2) {
-      setList3((list) => [...list, picLink]);
-    } else if (list3.length === 3) {
-      setList4((list) => [...list, picLink]);
-    } else if (list4.length === 4) {
-      setList5((list) => [...list, picLink]);
-    } else {
-      setList5((list) => [...list, picLink]);
+  const handleImgClick = useCallback((index, src) => {
+    if (count === 1) {
+      setList2((prevList) => [...prevList, "localhost:3000" + src]);
+    } 
+    else if (count===2){
+      setList3((prevList) => [...prevList, "localhost:3000" + src]);
+  }
+    else if (count === 0) {
+      setList((prevList) => [...prevList, "localhost:3000" + src]);
     }
-  };
-  
-  
+    else if (count === 3){
+      setList4((prevList) => [...prevList, "localhost:3000" + src]);
+    }
+    else if (count=== 4){
+      setList5((prevList) => [...prevList, "localhost:3000" + src]);
+    }
+    setSelectedImgIndex(index);
+    setCounting(() => 1);
+    // setImgIndexes(getRandomInts(4, picture.length-1));
+    // console.log(count);
+    // console.log(counting);
+    console.log(count);
+    console.log("List 1:", list);
+    console.log("List 2:", list2);
+    console.log("List 3:", list3);
+    console.log("List 4:", list4);
+    console.log("List 5:", list5);
+
+  }, [count, counting, picture.length, setList, setList2, setList3, setSelectedImgIndex, setCounting, setImgIndexes]);
+
+    
   
  
-  // const getRandomInts = (n, max) => {
-  //   const arr = [];
-  //   while (arr.length < n) {
-  //     const r = Math.floor(Math.random() * max);
-  //     if (!arr.includes(r)) {
-  //       arr.push(r);
-  //     }
-  //   }
-  //   return arr;
-  // };
-
+  const getRandomInts = (n, max) => {
+    const arr = [];
+    while (arr.length < n) {
+      const r = Math.floor(Math.random() * max);
+      if (!arr.includes(r)) {
+        arr.push(r);
+      }
+    }
+    return arr;
+  };
 
 
   useEffect(() => {
+    
     const interval = setInterval(() => {
       setCount((prevCount) => prevCount + 1);
       
     }, 5000);
-  
+
     if (count === 5) {
       clearInterval(interval);
     } else {
-      const newImgIndexes = [0,1,2,3];
-      setImgIndexes(newImgIndexes);
-      setSelectedImgIndex(-1); // reset selected image index
-      setCounting(0); // reset counting state
+      setImgIndexes(getRandomInts(4, picture.length - 1));
+      setSelectedImgIndex(-1);
     }
+
+    // Clear the orange border
+    setBorderIndex(-1);
+
     return () => clearInterval(interval);
-  }, [count, picture]);
-
+  }, [count,picture.length]);
 
   useEffect(() => {
+    // Your existing code...
+  
+    // Click on the first image after mounting the component
     if (selectedImgIndex === -1 && imgIndexes.length > 0) {
-      handleImgClick(4, picture[imgIndexes[4]]);
+      handleImgClick(0, picture[imgIndexes[0]]);
     }
+  
+    // Your existing code...
   }, [selectedImgIndex, imgIndexes, picture, handleImgClick]);
-
-  useEffect(() => {
-    if (count === 0){
-      console.log(list1);
-    }
-    else if (count === 1){
-      console.log(list2);
-    }
-    else if (count === 2){
-      console.log(list3);
-    }
-    else if (count === 3){
-      console.log(list4);
-    }
-    else if (count === 5){
-      console.log(list5);
-    };
-      
-  }, [list1,list2,list3,list4,list5]);
-
   
 
   return (
     <div className="h-[200px] grid grid-cols-2 gap-2 lg:gap-2 md:gap-3 content-around justify-items-center mt-71 sm:mt-[90px] md:mt-180 ml-16 mr-16">
-      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 0 && counting > 1  ? "border-4 border-orange" : ""}`}   onClick={() => handleImgClick(0, picture[imgIndexes[0]])} >
+      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 0 && counting > 1? "border-4 border-orange" : ""}`}   onClick={() => handleImgClick(0, picture[imgIndexes[0]])} >
         <img className="w-121 h-156 md:w-173 md:h-248 sm:w-[130px] sm:h-[180px]" src={picture[imgIndexes[0]]}  alt="pic1" />
       </div>
-      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 1 && counting > 1 ? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(1, picture[imgIndexes[1]])}>
+      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 1 && counting > 0 ? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(1, picture[imgIndexes[1]])}>
         <img className="w-121 h-156 md:w-173 md:h-248 sm:w-[130px] sm:h-[180px]" src={picture[imgIndexes[1]]} alt="pic2" />
       </div>
-      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 2 && counting > 1 ? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(2, picture[imgIndexes[2]])}>
+      <div className={`w-121 h-163 md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 2 && counting > 0 ? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(2, picture[imgIndexes[2]])}>
         <img className="w-121 h-156 md:w-173 md:h-248 sm:w-[130px] sm:h-[180px]" src={picture[imgIndexes[2]]} alt="pic3" />
       </div>
-      <div className={`w-121 h-[163px] md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 3 && counting > 1? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(3, picture[imgIndexes[3]])}>
+      <div className={`w-121 h-[163px] md:w-173 md:h-[255px] sm:w-[130px] sm:h-[180px] ${selectedImgIndex === 3 && counting > 0? "border-4 border-orange" : ""}`} onClick={() => handleImgClick(3, picture[imgIndexes[3]])}>
         <img className="w-121 h-156 md:w-173 md:h-248 sm:w-[130px] sm:h-[180px]" src={picture[imgIndexes[3]]} alt="pic4" />
-      </div>
-      <div className="h-0 w-0" >
-        <img className="w-0 h-0" src={picture[imgIndexes[4]]} alt="empty" onClick={() => handleImgClick(5, picture[imgIndexes[4]])}/>
       </div>
     </div>
   );
--- frontend/src/game/userinput.js
+++ frontend/src/game/userinput.js
@@ -1,56 +1,43 @@
 import React from 'react'
-
 import location from '../image/location.svg'
 import next from '../image/next.svg'
 import './main.css';
 
-import Urchoice from './urchoice';
-
-import { Link } from 'react-router-dom';
-
 import Toppage from './toppage';
 import TimeSlide from './timeslider';
 import Priceslider from './priceslider';
-import Foot from './foot'
+import Foot from './foot';
 
 export default function Userinput() {
 
-
+  function handleClick() {
+    if (window.liff) {
+      window.liff.closeWindow();
+    } else {
+      console.log('LINE SDK not initialized.');
+    }
+  }
 
   return (
-    <div className='h-screen ' >
-    
+    <div className='h-screen'>
       <Toppage />
-
       <div className="flex flex-col justify-center items-center ">
         <div className="relative bg-box p-4 rounded-xl w-[350px] h-68">
-
           <TimeSlide/>
           <Priceslider/>
-          
-
-
           <div className="flex mx-2 mt-3 ">
             <img src={location} alt={location} className="w-6 h-6 mr-4" />
             <p  className='mr-11 text-blue'>สถานที่</p>
           </div>
-
           <div className="search mt-2">
-          <input className=" appearance-none bg-darkyel border rounded-xl w-full py-2 px-3 text-krd leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Location"/>
+            <input className="appearance-none bg-darkyel border rounded-xl w-full py-2 px-3 text-krd leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Location"/>
           </div>
         </div>
-            
-        <Link to="/userinput2" className="bg-krd hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-[12px] w-20 h-10 inline-flex items-center mt-4">
+        <button className="bg-krd hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-[12px] w-20 h-10 inline-flex items-center mt-4" onClick={handleClick}>
           <span className="bg-no-repeat bg-center bg-cover w-4 h-4 ml-4 " style={{ backgroundImage: `url(${next})` }} />
- 
-        </Link>
+        </button>
       </div>
-
-      
-    <Foot/>
-   
-   
+      <Foot/>
     </div>
-
   )
 }
